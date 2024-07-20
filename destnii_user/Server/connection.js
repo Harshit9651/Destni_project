85845214c61cbcc92391c41e0b0693b571f4e6b4
Harshit sharma <programming966051@gmail.com>
2024-07-20 11:41:06 +0530

    changing controllers

--- destnii_user/Server/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Destni_user');
-  console.log("mongoose responsed sucessfully");
-}
-
-module.exports = main();
