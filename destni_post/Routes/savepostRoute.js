5b3d5891f51357fc3b1eb7043f839a4ce2217338
Harshit sharma <programming966051@gmail.com>
2024-07-30 15:04:42 +0530

    Activated  saved post in homepaoge section

--- destni_post/Routes/savepostRoute.js
+++ destni_post/Routes/savepostRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const savepostController = require("../controllers/savepostController");
+const{authenticateUser} = require("../Auth/authentication")
 router.get('/rendersavepage',savepostController.rendersavepostpage)
-router.post('/savepostdata',savepostController.savePost);
+router.post('/savepostdata/:postId',authenticateUser,savepostController.savePost);
 
 
 module.exports = router;
--- destni_post/controllers/savepostController.js
+++ destni_post/controllers/savepostController.js
@@ -15,29 +15,32 @@ exports.rendersavepostpage = async (req, res) => {
 
 exports.savePost = async (req, res) => {
   try {
-    const { userId, postId } = req.body;
-    console.log(userId, postId)
+    const postId = req.params.postId;
+    const userId = req.session.userId;
 
     if (!userId || !postId) {
       return res.status(statuscode.CLIENT_ERROR.BAD_REQUEST.code).json({ error: statusmessage.CLIENT_ERROR.BAD_REQUEST });
     }
 
-    const post = await Post.findOne({postId:postId});
+    const post = await Post.findOne({ _id: postId });
     if (!post) {
       return res.status(statuscode.CLIENT_ERROR.NOT_FOUND.code).json({ error: statusmessage.CLIENT_ERROR.NOT_FOUND });
     }
 
-    const existingSave = await SavePost.findOne({postId:postId});
+    const existingSave = await SavePost.findOne({ postId: postId, userId: userId });
     if (!existingSave) {
+      // Save the post
       const savepost = new SavePost({ userId, postId });
       const postsaved = await savepost.save();
       console.log(postsaved);
+      res.status(statuscode.SUCCESS.OK.code).json({ message: statusmessage.SUCCESS.OK });
     } else {
-      return res.status(statuscode.CLIENT_ERROR.CONFLICT.code).json({ error: statusmessage.CLIENT_ERROR.CONFLICT });
-   //yha hum userId ki jgh session.userId  dalenge ok
+      // Unsaved the post
+      const deletpostfromsave = await SavePost.findOneAndDelete({ postId: postId, userId: userId });
+      console.log(deletpostfromsave);
+      console.log("data deleted successfully");
+      res.status(statuscode.SUCCESS.OK.code).json({ message: statusmessage.SUCCESS.OK });
     }
-
-    res.status(statuscode.SUCCESS.OK.code).json({ message: statusmessage.SUCCESS.OK });
   } catch (error) {
     console.error('Error saving post:', error);
     res.status(statuscode.SERVER_ERROR.INTERNAL_SERVER_ERROR.code).json({ error: statusmessage.SERVER_ERROR.INTERNAL_SERVER_ERROR });
